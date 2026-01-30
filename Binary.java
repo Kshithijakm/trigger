0b4edc53453b0b52f9d6b48ad0a6baa65b582c9a
Kshithijakm <kshithijakm024@gmail.com>
2026-01-30 10:12:55 +0530

    Update Binary.java

--- Binary.java
+++ Binary.java
@@ -2,7 +2,7 @@ class Binary{
   public static void main(String[]args){
     int arr[]={1,2,3,4,5};
     int n=arr.length;
-    int target=3;
+    int target=6;
     int low=0;
     int high=n-1;
     int flag=0;
